15c81e9f5ea505f79ebc242d4627bfcc34fcd3b2
AyoubAitcheikhahmed <ayoub.aitcheikhahmed@student.hogent.be>
2024-11-04 22:20:26 +0100

    vercel config2

--- src/routes/default.route.js
+++ src/routes/default.route.js
@@ -1,10 +1,34 @@
 import express from "express";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
+const defaultRequestMapping = async (req, res) => {
 
-const defaultRequestMapping = (req, res) => {
-  res.send("Server is running");
+    const owners = await prisma.restaurantOwner.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        phone: true,
+        email: true,
+        address: true,
+        restaurants: {
+          select: {
+            name: true,
+            address: true,
+          }
+        }
+      }
+    });
+
+    const ownerDTOs = owners.map(owner => new RestaurantOwnerDTO(
+      owner.email || 'UNKNOWN',
+      owner.firstName || 'UNKNOWN',
+      owner.lastName || 'UNKNOWN'
+    ));
+
+    res.status(200).json(owner);
 };
 
 // Set up the route to use the register function
